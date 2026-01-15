82164a261f6368fbb4aebf51c2277fcf864f2867
m4ppy <leon.choi.dev@gmail.com>
2026-01-15 21:08:10 +0900

    initial setup

---
+++ src/main/java/com/leon/evention/EventionApplication.java
@@ -0,0 +1,13 @@
+package com.leon.evention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventionApplication.class, args);
+	}
+
+}
